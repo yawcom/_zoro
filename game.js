8b7fda13b45118007d9e855e8dc214b30186a4f7
yawcom <yawcom@hyperlabs.it>
2025-04-27 19:15:30 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -169,10 +169,8 @@ function startLevel() {
 
     //background = this.add.image(425, 425, 'background');
     //background.setDisplaySize(850, 850);
-	const width = this.scale.width;
-    const height = this.scale.height;
 	
-	background = this.add.image(width/2, height/2, 'background').setDisplaySize((config.height*1552)/2500, config.height);
+	background = this.add.image(this.scale.width/2, this.scale.height/2, 'background').setDisplaySize((config.height*1552)/2500, config.height);
     
     // Reset for new level
     foundCards = 0;
